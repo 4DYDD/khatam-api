abe71ace99d46cb9de8f1afc46e1446e599cf80c
Haidir Aditya <haidiraditya16@gmail.com>
2025-03-10 20:50:35 +0800

    ngetes API local di laravel untuk pertama kalinya

--- src/Web.jsx
+++ src/Web.jsx
@@ -1,13 +1,31 @@
-import { useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [datas, setDatas] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://api-nya.test/api/tes").then((response) => {
+      setDatas(response.data);
+    });
+  }, []);
 
   return (
     <>
-      <div className="h-screen flexc font-inter text-3xl font-semibold tracking-wide">
-        Halo Gais!
+      <div className="flex-col h-screen text-3xl font-semibold tracking-wide flexc font-inter">
+        <div>Halo Gais!</div>
+        <br />
+        {datas.length > 0 &&
+          datas.map((value, index) => (
+            <Fragment key={index}>
+              <ul className="mb-10">
+                <li className="text-base">{value.message}</li>
+                <li className="text-lg">{value.title}</li>
+                <li className="text-sm">{value.description}</li>
+              </ul>
+            </Fragment>
+          ))}
       </div>
     </>
   );
